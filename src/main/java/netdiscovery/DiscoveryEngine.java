a8b25f1a6af95dc629b8c45d16d221f7a032ff38
Cody Bumgardner <cbumgardner@gmail.com>
2016-02-14 16:57:49 -0500

    ff

--- src/main/java/netdiscovery/DiscoveryEngine.java
+++ src/main/java/netdiscovery/DiscoveryEngine.java
@@ -31,20 +31,20 @@ public class DiscoveryEngine implements Runnable
 	      socket = new DatagramSocket(32005, InetAddress.getByName("0.0.0.0"));
 	      socket.setBroadcast(true);
 	      
-	      System.out.println(getClass().getName() + ">>>Ready to receive broadcast packets!");
 	        
 	      PluginEngine.DiscoveryActive = true;
 	      while (PluginEngine.isActive) 
 	      {
-	        
+	    	  System.out.println(getClass().getName() + ">>>Ready to receive broadcast packets!");
+		        
 	        //Receive a packet
 	        byte[] recvBuf = new byte[15000];
 	        DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
 	        socket.receive(packet);
 
 	        //Packet received
-	        //System.out.println(getClass().getName() + ">>>Discovery packet received from: " + packet.getAddress().getHostAddress());
-	        //System.out.println(getClass().getName() + ">>>Packet received; data: " + new String(packet.getData()));
+	        System.out.println(getClass().getName() + ">>>Discovery packet received from: " + packet.getAddress().getHostAddress());
+	        System.out.println(getClass().getName() + ">>>Packet received; data: " + new String(packet.getData()));
 
 	        //See if the packet holds the right command (message)
 	        String message = new String(packet.getData()).trim();
