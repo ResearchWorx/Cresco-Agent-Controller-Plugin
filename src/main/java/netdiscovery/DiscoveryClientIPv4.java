b8cc3d388eacc753988658c7d97e3b3b4a69b9a7
vcbumg2 <cody@uky.edu>
2016-02-23 09:20:20 -0700

    ed

---
+++ src/main/java/netdiscovery/DiscoveryClientIPv4.java
@@ -0,0 +1,70 @@
+package netdiscovery;
+
+import java.net.InetAddress;
+import java.util.Map;
+
+import plugincore.PluginEngine;
+
+public class DiscoveryClientIPv4 
+{
+	//private int discoveryTimeout;
+	public DiscoveryClientIPv4()
+	{
+		//discoveryTimeout = Integer.parseInt(PluginEngine.config.getParam("discoverytimeout")); 
+		//System.out.println("DiscoveryClient : discoveryTimeout = " + discoveryTimeout);
+		//discoveryTimeout = 1000;
+	}
+	
+	public Map<String,String> getDiscoveryMap(int discoveryTimeout)
+	{
+		Map<String,String> disMap = null;
+		
+		try
+		{
+			while(PluginEngine.clientDiscoveryActive)
+			{
+				System.out.println("DiscoveryClient : Discovery already underway : waiting..");
+				Thread.sleep(2500);
+			}
+			PluginEngine.clientDiscoveryActive = true;
+			DiscoveryClientWorkerIPv4 dcw = new DiscoveryClientWorkerIPv4(discoveryTimeout);
+			disMap = dcw.getDiscoveryMap();
+		}
+		catch(Exception ex)
+		{
+			System.out.println("DiscoveryClient Error : " + ex.toString());
+		}
+		PluginEngine.clientDiscoveryActive = false;
+		
+		return disMap;
+	}
+	
+	public boolean isReachable(String hostname)
+	{
+		boolean reachable = false;
+		try
+		{
+		   //also, this fails for an invalid address, like "www.sjdosgoogle.com1234sd" 
+	       //InetAddress[] addresses = InetAddress.getAllByName("www.google.com");
+			InetAddress address =  InetAddress.getByName(hostname);
+	      
+	        if (address.isReachable(10000))
+	        {   
+	        	reachable = true;
+	        }
+	        else
+	        {
+	           reachable = false;
+	        }
+	      
+		}
+		catch(Exception ex)
+		{
+			System.out.println("DiscoveryClient : isReachable : Error " + ex.toString());
+		}
+		return reachable;
+	}
+	
+
+
+}
