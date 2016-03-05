000e93c85872cb81064f14a75cf77d91aeecdd62
vcbumg2 <cody@uky.edu>
2016-03-05 11:22:28 -0500

    cd

--- src/main/java/netdiscovery/DiscoveryEngine.java
+++ src/main/java/netdiscovery/DiscoveryEngine.java
@@ -125,7 +125,7 @@ public class DiscoveryEngine implements Runnable
 		  	 		        DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
 		  	 		        
 		  	 		        socket.receive(packet); //rec broadcast packet, could be IPv6 or IPv4
-		  	 		          
+		  	 		        System.out.println("INCOMING PACKET!!!!");
 		  	 		        new Thread(new DiscoveryResponder(socket,packet)).start();
 		  	 		        
 		  	 	    		}
