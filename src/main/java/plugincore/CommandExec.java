f8acbd9446124dbabd1d12ba15bec07bf9c58736
vcbumg2 <cody@uky.edu>
2016-03-28 14:46:10 -0400

    wroking with message send

--- src/main/java/plugincore/CommandExec.java
+++ src/main/java/plugincore/CommandExec.java
@@ -17,6 +17,7 @@ public class CommandExec {
 
 	public MsgEvent cmdExec(MsgEvent ce) 
 	{
+		System.out.println("MESSAGE IN CONTROLLER: " + ce.getParams());
 		try
 		{
 			if(ce.getMsgType() == MsgEventType.CONFIG) //for init
@@ -42,77 +43,31 @@ public class CommandExec {
 				//PluginEngine.rpcMap.put(callId, ce);
 				return null;
 			}
-			else if((ce.getParam("dst_region") != null) && (ce.getParam("dst_agent") != null) && (ce.getParam("dst_plugin") != null)) //its a message for this plugin
+			else if((ce.getMsgRegion().equals(PluginEngine.region) && (ce.getMsgAgent().equals(PluginEngine.agent)) && (ce.getMsgPlugin().equals(PluginEngine.plugin))))
 			{
-				if((ce.getParam("dst_region").equals(PluginEngine.region)) && (ce.getParam("dst_agent").equals(PluginEngine.agent)) && (ce.getParam("dst_plugin").equals(PluginEngine.plugin)))
-				{	
-					//message is for this plugin!
-					if(ce.getMsgType() == MsgEventType.DISCOVER)
-					{
-						//ce.setParams(PluginEngine.config.getPluginConfig());
-						/*
-						StringBuilder sb = new StringBuilder();
-						sb.append("help\n");
-						sb.append("show\n");
-						sb.append("show_name\n");
-						sb.append("show_version\n");
-						sb.append("From AMPQ\n");
-						
-						ce.setMsgBody(sb.toString());
-						*/
-						ce.setMsgBody(PluginEngine.config.getPluginConfigString());
-						
-					}
-					else if(ce.getMsgType() == MsgEventType.EXEC) //Execute and respond to execute commands
-					{
-						if(ce.getParam("cmd").equals("show") || ce.getParam("cmd").equals("?") || ce.getParam("cmd").equals("help"))
-						{
-						StringBuilder sb = new StringBuilder();
-						sb.append("\nPlugin " + PluginEngine.pluginName + " Help\n");
-						sb.append("-\n");
-						sb.append("show\t\t\t\t\t Shows Commands\n");
-						sb.append("show name\t\t\t\t Shows Plugin Name\n");
-						sb.append("show version\t\t\t\t Shows Plugin Version");
-						ce.setMsgBody(sb.toString());
-						}
-						else if(ce.getParam("cmd").equals("show_version"))
-						{
-							ce.setMsgBody(PluginEngine.pluginVersion);
-						}
-						else if(ce.getParam("cmd").equals("show_name"))
-						{
-							ce.setMsgBody(PluginEngine.pluginName);
-						}
-						/*
-						else if(ce.getParam("cmd").equals("enablelogconsumer"))
-						{
-							//ce.setMsgBody(PluginEngine.pluginName);
-							PluginEngine.enableLogConsumer(true);
-							return null;
-						}
-						*/
-					}
-					else
-					{
-						ce.setMsgBody("Dummy Plugin Command [" + ce.getMsgType().toString() + "] unknown");
+
+				if((ce.getParam("dst_region") != null) && (ce.getParam("dst_agent") != null) && (ce.getParam("dst_plugin") != null)) //plugin message
+				{
+					if((ce.getParam("dst_region").equals(PluginEngine.region)) && (ce.getParam("dst_agent").equals(PluginEngine.agent)) && (ce.getParam("dst_plugin").equals(PluginEngine.plugin))) {
+						logger.debug("MESSAGE FOR THIS PLUGIN");
+						return null;
 					}
 				}
-				else if((ce.getParam("dst_region").equals(PluginEngine.region)) && (ce.getParam("dst_agent").equals(PluginEngine.agent)) && (!ce.getParam("dst_plugin").equals(PluginEngine.plugin)))
+				if((ce.getParam("dst_region") != null) && (ce.getParam("dst_agent") != null) && (ce.getParam("dst_plugin") == null)) { //agent message
+					logger.debug("OUTGOING MESSAGE FOR EXTERNAL AGENT");
+				}
+			}
+			else if((ce.getMsgRegion().equals(PluginEngine.region) && (ce.getMsgAgent().equals(PluginEngine.agent)) && (ce.getMsgPlugin() == null))) //message for this agent
+			{
+				if((ce.getParam("dst_region").equals(PluginEngine.region)) && (ce.getParam("dst_agent").equals(PluginEngine.agent)))
 				{
 					//message for plugin send to agent
+					logger.debug("MESSAGE FOR THIS AGENT");
 					PluginEngine.msgInQueue.offer(ce);
-				}
-				else
-				{
-					ce.setMsgPlugin(ce.getParam("dst_plugin"));
-					ce.setMsgAgent(ce.getParam("dst_agent"));
-					//MsgEvent cr = PluginEngine.acc.call(ce);
-					//return cr;
-					//call remote
 					return null;
 				}
-			
 			}
+			//
 			else if((ce.getParam("dst_region") != null) && (ce.getParam("dst_agent") != null)) //its a message for this plugin
 			{
 				//(isReachableAgent(targetAgent))
