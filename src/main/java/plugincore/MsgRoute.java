378410c21c2980438c1534f56fd93107f1f08ab9
vcbumg2 <cody@uky.edu>
2016-04-01 09:58:10 -0400

    Blitzed burger

--- src/main/java/plugincore/MsgRoute.java
+++ src/main/java/plugincore/MsgRoute.java
@@ -29,7 +29,7 @@ public class MsgRoute implements Runnable{
                  }
                  break;
              case 52:  //System.out.println("CONTROLLER ROUTE TO REGIONAL AGENT : 52 " + rm.getParams()); //also where regional messages go
-                 if((PluginEngine.isRegionalController) && (rm.getParam("dst_agent") == null)) {
+                 if((PluginEngine.isRegionalController) && (rm.getParam("dst_agent") == null)) { //if this is the regional controller consume the message
                     System.out.println("EAT REGIONAL MESSAGE");
                  }
                  else {
