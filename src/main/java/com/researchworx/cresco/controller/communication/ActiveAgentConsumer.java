9bbfd9ac5c01a6308965de7c0b157acb1566ba88
Cody Bumgardner <cbumgardner@gmail.com>
2016-05-19 10:14:07 -0400

    DiscoveryEngine : Add logger to threads

--- src/main/java/com/researchworx/cresco/controller/communication/ActiveAgentConsumer.java
+++ src/main/java/com/researchworx/cresco/controller/communication/ActiveAgentConsumer.java
@@ -20,7 +20,7 @@ public class ActiveAgentConsumer implements Runnable {
 
 	public ActiveAgentConsumer(Launcher plugin, String RXQueueName, String URI, String brokerUserNameAgent, String brokerPasswordAgent) {
 		logger.debug("Agent Consumer initialized : Queue : " + RXQueueName);
-        logger.trace("RXQueue=" + RXQueueName + " URI=" + URI + " brokerUserNameAgent=" + brokerPasswordAgent + " brokerPasswordAgent=" + brokerPasswordAgent);
+        logger.trace("RXQueue=" + RXQueueName + " URI=" + URI + " brokerUserNameAgent=" + brokerUserNameAgent + " brokerPasswordAgent=" + brokerPasswordAgent);
 		this.plugin = plugin;
 		try {
 			conn = (ActiveMQConnection)new ActiveMQConnectionFactory(brokerUserNameAgent,brokerPasswordAgent,URI).createConnection();
