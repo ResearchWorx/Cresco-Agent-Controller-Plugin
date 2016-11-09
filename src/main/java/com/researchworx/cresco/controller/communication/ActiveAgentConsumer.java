45f6b6a01dde71b073977fd19934c53412a1106d
Cody Bumgardner <cbumgardner@gmail.com>
2016-11-09 09:53:03 -0500

    BDB

--- src/main/java/com/researchworx/cresco/controller/communication/ActiveAgentConsumer.java
+++ src/main/java/com/researchworx/cresco/controller/communication/ActiveAgentConsumer.java
@@ -12,24 +12,27 @@ import javax.jms.*;
 public class ActiveAgentConsumer implements Runnable {
 	private Launcher plugin;
 	private CLogger logger;
-	private Queue RXqueue; 
+	private Queue RXqueue;
 	private Session sess;
 	private ActiveMQConnection conn;
 
-	public ActiveAgentConsumer(Launcher plugin, String RXQueueName, String URI, String brokerUserNameAgent, String brokerPasswordAgent) {
+	public ActiveAgentConsumer(Launcher plugin, String RXQueueName, String URI, String brokerUserNameAgent, String brokerPasswordAgent) throws JMSException {
 		logger = new CLogger(ActiveAgentConsumer.class, plugin.getMsgOutQueue(), plugin.getRegion(), plugin.getAgent(), plugin.getPluginID());
 		logger.debug("Queue: {}", RXQueueName);
 		logger.trace("RXQueue=" + RXQueueName + " URI=" + URI + " brokerUserNameAgent=" + brokerUserNameAgent + " brokerPasswordAgent=" + brokerPasswordAgent);
 		this.plugin = plugin;
 		int retryCount = 10;
-		while (((conn == null) || !conn.isStarted()) && retryCount-- > 0) {
-			try {
-				conn = (ActiveMQConnection) new ActiveMQConnectionFactory(brokerUserNameAgent, brokerPasswordAgent, URI).createConnection();
-				conn.start();
-				sess = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-				RXqueue = sess.createQueue(RXQueueName);
-				break;
-			} catch (JMSException je) {
+
+		//while (((conn == null) || !conn.isStarted()) && retryCount-- > 0) {
+		//try {
+		conn = (ActiveMQConnection) new ActiveMQConnectionFactory(brokerUserNameAgent, brokerPasswordAgent, URI).createConnection();
+		conn.start();
+		sess = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		RXqueue = sess.createQueue(RXQueueName);
+		//break;
+		//}
+			/*
+			catch (JMSException je) {
 				try {
 				    logger.error("JMSException: {}", je.getMessage());
 					logger.debug("brokerUserNameAgent={}, brokerPasswordAgent={},URI={}", brokerUserNameAgent, brokerPasswordAgent, URI);
@@ -38,11 +41,16 @@ public class ActiveAgentConsumer implements Runnable {
 				} catch (InterruptedException e) {
 					logger.trace("Initialization was interrupted");
 				}
-			} catch (Exception ex) {
-				logger.error("Constructor: {}", ex.getMessage());
-				break;
 			}
-		}
+
+			catch (Exception ex) {
+				logger.error("Constructor: {}", ex.getMessage());
+				//break;
+
+			}
+			*/
+
+		//}
 	}
 
 	@Override
@@ -60,11 +68,11 @@ public class ActiveAgentConsumer implements Runnable {
 					plugin.msgIn(me);
 					logger.debug("Message: {}", me.getParams().toString());
 				}
-                else {
+				else {
 					logger.trace("Queue Details: {}", RXqueue);
 					logger.trace("isStarted=" + conn.isStarted() + " isClosed=" + conn.isClosed() + " ClientId=" + conn.getInitializedClientID());
 					logger.trace("brokerName=" + conn.getBrokerName() + " username=" + conn.getConnectionInfo().getUserName() + " password=" + conn.getConnectionInfo().getPassword());
-                }
+				}
 			}
 			logger.debug("Cleaning up");
 			sess.close();
