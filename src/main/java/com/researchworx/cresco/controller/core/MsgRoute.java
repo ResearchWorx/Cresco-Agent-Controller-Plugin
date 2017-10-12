a4f508ca92caf95efd191c0d26413d248c4e8b52
codybum <codeman01>
2017-10-12 14:52:36 -0400

    Bump up DB version

--- src/main/java/com/researchworx/cresco/controller/core/MsgRoute.java
+++ src/main/java/com/researchworx/cresco/controller/core/MsgRoute.java
@@ -22,8 +22,9 @@ public class MsgRoute implements Runnable {
 
             int routePath = getRoutePath();
 
-            if((routePath == 112) && (rm.getMsgType() == MsgEvent.Type.CONFIG)) {
-                logger.error("CASE=" + routePath + " GC=" + plugin.isGlobalController() + " RC=" + plugin.isRegionalController());
+            //if((routePath == 112) && (rm.getMsgType() == MsgEvent.Type.CONFIG)) {
+                if(rm.getMsgType() == MsgEvent.Type.CONFIG) {
+                    logger.error("CASE=" + routePath + " GC=" + plugin.isGlobalController() + " RC=" + plugin.isRegionalController());
                 logger.error(rm.getParams().toString());
                 logger.error(rm.getMsgType().toString());
             }
