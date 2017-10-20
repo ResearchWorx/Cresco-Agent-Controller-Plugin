0aa3c44d42f8a72193727a3ecdbeba3c14d89067
codybum <codeman01>
2017-10-20 10:18:03 -0400

    Bump up DB version

--- src/main/java/com/researchworx/cresco/controller/regionalcontroller/RegionalCommandExec.java
+++ src/main/java/com/researchworx/cresco/controller/regionalcontroller/RegionalCommandExec.java
@@ -107,7 +107,9 @@ public class RegionalCommandExec {
 			}
 			else if(le.getMsgType() == MsgEvent.Type.WATCHDOG) {
 				regionalDiscovery.discover(le);
-				logger.error("Headed to regional discovery.");
+				if(le.getParam("pluginconfigs") != null) {
+                    logger.error("Headed to regional discovery.");
+                }
 
         }
             else if (le.getMsgType() == MsgEvent.Type.INFO) {
