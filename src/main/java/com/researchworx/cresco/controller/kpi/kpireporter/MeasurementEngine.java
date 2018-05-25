2d7edda3b09e62c5a3e43b0d11f369b46a396fed
Cody Bumgardner <cody@uky.edu>
2018-05-25 15:12:21 -0400

    Adding performance counters

--- src/main/java/com/researchworx/cresco/controller/kpi/kpireporter/MeasurementEngine.java
+++ src/main/java/com/researchworx/cresco/controller/kpi/kpireporter/MeasurementEngine.java
@@ -1,14 +1,18 @@
 package com.researchworx.cresco.controller.kpi.kpireporter;
 
+import com.codahale.metrics.JmxReporter;
+import com.codahale.metrics.MetricRegistry;
 import com.researchworx.cresco.controller.core.Launcher;
 import com.researchworx.cresco.library.utilities.CLogger;
 import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
 import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
 import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.util.HierarchicalNameMapper;
 import io.micrometer.jmx.JmxConfig;
 import io.micrometer.jmx.JmxMeterRegistry;
 
@@ -25,7 +29,15 @@ public class MeasurementEngine {
         this.plugin = plugin;
         this.logger = new CLogger(MeasurementEngine.class, plugin.getMsgOutQueue(), plugin.getRegion(), plugin.getAgent(), plugin.getPluginID(),CLogger.Level.Info);
 
-        this.jmxMeterRegistry = new JmxMeterRegistry(JmxConfig.DEFAULT, Clock.SYSTEM);
+
+        MetricRegistry metricRegistry = new MetricRegistry();
+        JmxReporter jmxReporter = JmxReporter.forRegistry(metricRegistry)
+                .inDomain("cresco")
+                .build();
+
+        jmxMeterRegistry = new JmxMeterRegistry(JmxConfig.DEFAULT, Clock.SYSTEM, HierarchicalNameMapper.DEFAULT, metricRegistry, jmxReporter);
+
+        //this.jmxMeterRegistry = new JmxMeterRegistry(JmxConfig.DEFAULT, Clock.SYSTEM);
 
         new ClassLoaderMetrics().bindTo(jmxMeterRegistry);
         new JvmMemoryMetrics().bindTo(jmxMeterRegistry);
